51056376374d96cd04a1a79e22a372ffa6a5e018
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-14 22:02:08 -0300

    codigo revisado

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from skimage.metrics import mean_squared_error
 
 
 # diretorios ----------------------------------------------------
-RAW_DIR = 'UIEB_Raw'
-REF_DIR = 'UIEB_Reference'
+RAW_DIR = 'raw-890'
+REF_DIR = 'reference-890'
 OUTPUT_DIR = 'output/'
 CSV_FILENAME = 'metrics_results.csv'
 
@@ -18,85 +18,103 @@ if not os.path.exists(OUTPUT_DIR):
 
 #----------------------------------------------------------------
 
-# recebe a matriz de pixels diretamente
 
 def dac_implementation(img):
 
-    # Define tamanho max para o mean filter
+    # Define tamanho max para o Mean filter
     hight, weight = img.shape[:2]
     max_size = max(hight, weight)  
 
-    # opencv exige que a matriz seja impar
+    # matriz deve ser impar
     if max_size % 2 == 0:
         max_size +=1
 
     # OpenCV le na ordem BGR
     b_channel, g_channel, r_channel = cv2.split(img)
 
-    # Utiliza CLAHE em cada channel para aumentar constraste da imagem
+
+    # Utiliza CLAHE em cada channel para aumentar constraste da imagem 
+
     # Arigo omite os valores de limite e tamanho do bloco
-    clahe = cv2.createCLAHE(clipLimit=1.1, tileGridSize=(8,8))   # testar os valores
-    red_enhanced = clahe.apply(r_channel)
-    green_enhanced = clahe.apply(g_channel)
-    blue_enhanced = clahe.apply(b_channel)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))   
+    red_clahe = clahe.apply(r_channel)
+    green_clahe = clahe.apply(g_channel)
+    blue_clahe = clahe.apply(b_channel)
 
-    # converte para float para manter os valores negativos
+
+    # Blue_clahe = Base Layer Blue + Detail Layer Blue (Green is the same thing )
+
     # artigo tambem nao fala os falores do filtro bilateral
-    r_enhanced = cv2.bilateralFilter(red_enhanced.astype(np.float32), d=9, sigmaColor=75, sigmaSpace=75)
-    g_enhanced = cv2.bilateralFilter(green_enhanced.astype(np.float32), d=9, sigmaColor=75, sigmaSpace=75)
-    b_enhanced = cv2.bilateralFilter(blue_enhanced.astype(np.float32), d=9, sigmaColor=75, sigmaSpace=75)
+    r_bilateral = cv2.bilateralFilter(red_clahe, d=1, sigmaColor=1, sigmaSpace=1)
+    g_bilateral = cv2.bilateralFilter(green_clahe, d=1, sigmaColor=1, sigmaSpace=1)
+    b_bilateral = cv2.bilateralFilter(blue_clahe, d=1, sigmaColor=1, sigmaSpace=1)
 
+
+    # converte para float32 para fazer a subtração e possuir valores negativos
+    r_bilateral = r_bilateral.astype(np.float32)
+    g_bilateral = g_bilateral.astype(np.float32)
+    b_bilateral = b_bilateral.astype(np.float32)
 
     # Separar a Base Layer da Detail Layer de cada canal (R nao vai ter detail layer)
     # mean filter é o cv2.blur
-    r_baseLayer = cv2.blur(r_enhanced, (max_size, max_size))
-    g_baseLayer = cv2.blur(g_enhanced, (max_size, max_size))
-    b_baseLayer = cv2.blur(b_enhanced, (max_size, max_size))
+    r_baseLayer = cv2.blur(r_bilateral, (max_size, max_size))
+    g_baseLayer = cv2.blur(g_bilateral, (max_size, max_size))
+    b_baseLayer = cv2.blur(b_bilateral, (max_size, max_size))
 
-    g_detailLayer = g_enhanced - g_baseLayer
-    b_detailLayer = b_enhanced - b_baseLayer
 
-    # Criar Detail Layer do canal R a partir da Base Layer R e Detail Layer do G e B  (Criando o R channel completo)
-    g_average = np.mean(g_enhanced)
-    b_average = np.mean(b_enhanced)
+    # Detail Layer do Green e Blue
+    g_detailLayer = g_bilateral - g_baseLayer
+    b_detailLayer = b_bilateral - b_baseLayer
+
+    # meadia para conferir qual canal tem mais predominancia na imagem     
+    g_average = np.mean(g_bilateral)
+    b_average = np.mean(b_bilateral)
 
     # se a imagem conter mais azul, use o detail layer do azul, se verde for predominante use o detail layer do verde
-    # r_detailLayer é o r_cp (compensation)
+    # r_detailLayer é o Rcp (R compensation)
     if(g_average > b_average):
         # verde predominante
-        r_detailLayer = cv2.add(r_baseLayer, g_detailLayer)
+        Rcp = r_baseLayer + g_detailLayer
 
     else:
         # azul predominante
-        r_detailLayer = cv2.add(r_baseLayer, b_detailLayer)
+        Rcp = r_baseLayer + b_detailLayer
 
 
-    # elimina valores negativos antes do gray world
-    r_detailLayer = np.clip(r_detailLayer, 0.0, 255.0)
-
+    # Rcp contém todo o R Channel
 
     # Utiliza Gray World Algorithm para corrigir as cores
-    r_average = np.mean(r_detailLayer)
+
+
+    # media do R channel
+    r_average = np.mean(Rcp)
+
+    # FORMULA GRAY
     gray = (r_average + g_average + b_average) / 3
 
     # K é o scale parameter 
+    # FORMULA Kr
     k_r = 0.8 * (gray / r_average)   # 0.8 é o valor alfa dado no artigo
+
+    # FORMULA Kc  
     k_g = gray / g_average
     k_b = gray / b_average
 
-    #DEBUG
-    #print(f"g_avg={g_average:.2f} b_avg={b_average:.2f} r_avg={r_average:.2f} gray={gray:.2f}")
-    #print(f"k_r={k_r:.2f} k_g={k_g:.2f} k_b={k_b:.2f}")
 
+    # Multiplicações Gray World
+    r_gw = k_r * Rcp 
+    g_gw = k_g * g_bilateral 
+    b_gw = k_b * b_bilateral
 
-    r_output = np.clip(k_r * r_detailLayer, 0, 255).astype(np.uint8)
-    # saida do G e B a formula utiliza enhanced
-    g_output = np.clip(k_g * g_enhanced, 0, 255).astype(np.uint8)
-    b_output = np.clip(k_b * b_enhanced, 0, 255).astype(np.uint8)
+    # FORMULA IRout
+    r_out = np.clip(r_gw, 0, 255).astype(np.uint8)
+
+    # FORMULA ICout
+    g_out = np.clip(g_gw, 0, 255).astype(np.uint8)
+    b_out = np.clip(b_gw, 0, 255).astype(np.uint8)
 
     # Juntar tudo (BGR)
-    #final_image = cv2.merge((b_output, g_output, r_output))
-    final_image = cv2.merge((b_output, g_output, r_output))
+    final_image = cv2.merge((b_out, g_out, r_out))
 
     return final_image
 
